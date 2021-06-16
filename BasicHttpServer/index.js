62136e0256f1e17197b5a51d238ae247fcdcd68c
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-16 13:33:39 +0530

    added requestHandler , sending response to the browser

--- BasicHttpServer/index.js
+++ BasicHttpServer/index.js
@@ -1,8 +1,13 @@
 const http = require("http");
 const port = 8000;
 
+function requestHandler(req,res){
+    res.writeHead(200,{'content-type':'text/html'})//Telling browser the type of content we are sending
+    res.end('<h1>Sending Response !!</h1>');
+}
+
 //Creating Server
-const server = http.createServer();
+const server = http.createServer(requestHandler);
 
 //Go to Port
 server.listen(port, function (err) {
