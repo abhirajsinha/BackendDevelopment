507739a55ad72c2931244cb23598191be7103e8e
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-16 14:08:25 +0530

    Extending to Multiple pages

--- BasicHttpServer/index.js
+++ BasicHttpServer/index.js
@@ -5,15 +5,27 @@ const fs = require("fs"); //Read or write from file
 function requestHandler(req, res) {
   res.writeHead(200, { "content-type": "text/html" }); //Telling browser the type of content we are sending
 
-  fs.readFile("./index.html", function (err, data) {
+  let filePath;
+  switch (req.url) {
+    case "/":
+      filePath = "./index.html";
+      break;
+    case "/profile":
+      filePath = "./profile.html";
+      break;
+
+    default:
+      filePath = "./404.html";
+  }
+
+  fs.readFile(filePath, function (err, data) {
     if (err) {
-      console.log("Error", err);
+      console.log("Error!", err);
       return res.end("<h1>Error!</h1>");
     }
+
     return res.end(data);
   });
-
-//   res.end("<h1>Sending Response !!</h1>");
 }
 
 //Creating Server
