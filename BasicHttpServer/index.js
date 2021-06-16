24f2d1a15f06f34a098b65474856095d8fea2279
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-16 13:48:30 +0530

    use fs module and added external html file to view changes via response

--- BasicHttpServer/index.js
+++ BasicHttpServer/index.js
@@ -1,9 +1,19 @@
 const http = require("http");
 const port = 8000;
+const fs = require("fs"); //Read or write from file
 
-function requestHandler(req,res){
-    res.writeHead(200,{'content-type':'text/html'})//Telling browser the type of content we are sending
-    res.end('<h1>Sending Response !!</h1>');
+function requestHandler(req, res) {
+  res.writeHead(200, { "content-type": "text/html" }); //Telling browser the type of content we are sending
+
+  fs.readFile("./index.html", function (err, data) {
+    if (err) {
+      console.log("Error", err);
+      return res.end("<h1>Error!</h1>");
+    }
+    return res.end(data);
+  });
+
+//   res.end("<h1>Sending Response !!</h1>");
 }
 
 //Creating Server
@@ -11,10 +21,10 @@ const server = http.createServer(requestHandler);
 
 //Go to Port
 server.listen(port, function (err) {
-    if(err){
-        console.log("Error!!!",err);
-        return;
-    }
+  if (err) {
+    console.log("Error!!!", err);
+    return;
+  }
 
-    console.log("Server is Up and Running and Port: ",port);
+  console.log("Server is Up and Running and Port: ", port);
 });
