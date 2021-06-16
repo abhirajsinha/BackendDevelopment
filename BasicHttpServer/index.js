7282b31a4645fec73cbf9c565c912f601c269d13
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-16 13:19:59 +0530

    added nodejs server, connected server

--- BasicHttpServer/index.js
+++ BasicHttpServer/index.js
@@ -1,1 +1,15 @@
-HTMLOptionsCollection
+const http = require("http");
+const port = 8000;
+
+//Creating Server
+const server = http.createServer();
+
+//Go to Port
+server.listen(port, function (err) {
+    if(err){
+        console.log("Error!!!",err);
+        return;
+    }
+
+    console.log("Server is Up and Running and Port: ",port);
+});
