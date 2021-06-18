de39af593424727ec1abde43ba8ba1d4f82d2a95
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-18 19:49:37 +0530

    add

--- contact_list/index.js
+++ contact_list/index.js
@@ -3,6 +3,9 @@ const port=8000;
 
 const app=express();
 
+app.get('/',function(req,res){
+    res.send('<h1>Cool !, It is Running</h1>');
+})
 
 app.listen(port,function(err){
     if(err){
@@ -11,3 +14,4 @@ app.listen(port,function(err){
 
     console.log('Express Server is Running!',port);
 });
+sss
