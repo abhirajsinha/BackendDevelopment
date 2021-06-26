4d0170b0c1488750cc4dfbdf0b55b3ac0780bc50
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-26 08:58:49 +0530

    Setup template engine added contacts list

--- contact_list/index.js
+++ contact_list/index.js
@@ -7,9 +7,33 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+var contactList = [
+  {
+    name: "Aman",
+    phone: "1243454",
+  },
+  {
+    name: "Abhi",
+    phone: "11143454",
+  },
+  {
+    name: "Rishav",
+    phone: "333243454",
+  },
+];
+
 app.get("/", function (req, res) {
-  //   res.send("<h1>Cool !, It is Running</h1>");
-  return res.render('home');
+  return res.render("home", { 
+    title: "My Contacts List" ,
+    contact_list: contactList
+  });
+  
+});
+
+app.get("/practice", function (req, res) {
+  return res.render("practice", {
+    title: "Play with EJs",
+  });
 });
 
 app.listen(port, function (err) {
